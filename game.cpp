a8c21057056a29598a76128be8d21a9ff8a5dd8c
최지웅 <dbxhvldk8160@naver.com>
2024-04-16 17:48:46 +0900

    fix:Tetromino

--- game.cpp
+++ game.cpp
@@ -1,55 +1,107 @@
 #include "game.h"
 #include "tetromino.h"
 #include "./console/console.h"
-
+#include <string>
 // 게임 판을 나타내는 배열
 // board[x][y]가 true 인 경우 x, y 위치에 고정된 블록이 존재하는 것을 의미한다
 bool board_[BOARD_WIDTH][BOARD_HEIGHT];
 
-Tetromino I = Tetromino::I;
-Tetromino O = Tetromino::O;
-Tetromino T = Tetromino::T;
-Tetromino S = Tetromino::S;
-Tetromino Z = Tetromino::Z;
-Tetromino J = Tetromino::J;
-Tetromino L = Tetromino::L;
+Tetromino objectTetromino = Tetromino::I;
+Tetromino originalTetromino = Tetromino::I;
+Tetromino nextTetromino = Tetromino::I;
 
-// 키를 입력받는다.
-void handleInput() {
-  if (console::key(console::K_LEFT)) {
-    
-  }
-  if (console::key(console::K_RIGHT)) {
-    
-  }
-  if (console::key(console::K_UP)) {
-    
-  }
-  if (console::key(console::K_DOWN)) {
-    
-  }
-  if (console::key(console::K_Z)) {
-    
-  }
-  if (console::key(console::K_X)) {
-    
-  }
-  if (console::key(console::K_SPACE)) {
-    
-  }
+int xTetromino = BOARD_WIDTH/2;
+int yTetromino = 1;
+int dropTimer = DROP_DELAY;
+
+// board_를 초기화한다.
+void Game::setBoard() {
+    for (int i = 0; i < BOARD_WIDTH; i++) {
+        for (int j = 0; j < BOARD_HEIGHT; j++) {
+            board_[i][j] = false;
+        }
+    }
+}
+
+// 위쪽 방향키를 누르면 테트로미노를 맨 아래로 이동한다. (상 방향키 입력)
+void Game::hardDrop() {
+
+}
+
+// 테트로미노를 일정 간격으로 아래로 이동
+void Game::softDrop() {
+    dropTimer--;
+    if (dropTimer == 0) {
+        dropTimer = DROP_DELAY;
+        yTetromino += 1;
+    }
+}
+
+// board를 체크하여 가로 한 줄이 꽉 찼으면 삭제 clearLine에 1을 추가
+void Game::isClearLine() {
+
+}
+
+// clearLine이 일정 개수에 도달하면 게임 종료 (gameClear = True)
+void Game::clearGame() {
+
+}
+
+// 테트로미노를 시계, 반시계 방향으로 회전한다. (Z, X키 입력)
+void Game::rotatedTetromino() {
+    //moveTetromino()에 임시 통합 (정상 작동 확인)
+}
+
+// 테트로미노를 이동한다. (좌, 우, 하 방향키 입력)
+void Game::moveTetromino() {
+    if (console::key(console::K_LEFT)) {
+        xTetromino -= 1;
+    }
+    if (console::key(console::K_RIGHT)) {
+        xTetromino += 1;
+    }
+    if (console::key(console::K_UP)) {
+        //
+    }
+    if (console::key(console::K_DOWN)) {
+        yTetromino += 1;
+    }
+    if (console::key(console::K_Z)) {
+        objectTetromino = objectTetromino.rotatedCW();
+    }
+    if (console::key(console::K_X)) {
+        objectTetromino = objectTetromino.rotatedCCW();
+    }
+    if (console::key(console::K_SPACE)) {
+        createTetromino();
+    }
+}
+
+// 테트로미노를 생성한다.
+void Game::createTetromino() {
+    xTetromino = BOARD_WIDTH / 2;
+    yTetromino = 1;
+    int selectTetromino = rand() % 7;
+    if (selectTetromino == 0) objectTetromino = Tetromino::I;
+    else if (selectTetromino == 1) objectTetromino = Tetromino::O;
+    else if (selectTetromino == 2) objectTetromino = Tetromino::T;
+    else if (selectTetromino == 3) objectTetromino = Tetromino::S;
+    else if (selectTetromino == 4) objectTetromino = Tetromino::Z;
+    else if (selectTetromino == 5) objectTetromino = Tetromino::J;
+    else if (selectTetromino == 6) objectTetromino = Tetromino::L;
 }
 
 // 게임의 한 프레임을 처리한다.
 void Game::update() {
-    
+    softDrop();
+    moveTetromino();
 }
 
 // 게임 화면을 그린다.
 void Game::draw() {
     console::drawBox(0, 0, BOARD_WIDTH + 1, BOARD_HEIGHT + 1);
-    I.drawAt(BLOCK_STRING, BOARD_WIDTH/2 , 1);
-    Tetromino Icw = I.rotatedCW();
-    Icw.drawAt(BLOCK_STRING, BOARD_WIDTH/2 , 6);
+    objectTetromino.drawAt(BLOCK_STRING, xTetromino, yTetromino);
+    objectTetromino.drawAt(SHADOW_STRING, xTetromino, BOARD_HEIGHT-1);
 }
 
 // 게임 루프가 종료되어야 하는지 여부를 반환한다.
@@ -62,6 +114,6 @@ bool Game::shouldExit() {
 }
 
 Game::Game() {
-    
-
+    setBoard();
+    createTetromino();
 }
--- game.h
+++ game.h
@@ -1,9 +1,9 @@
-
 #ifndef GAME_H
 #define GAME_H
 
 #include "tetromino.h"
-
+#include "./console/console.h"
+#include <string>
 #define BOARD_WIDTH 10
 #define BOARD_HEIGHT 20
 #define LINES 40
@@ -15,10 +15,41 @@ private:
   // 게임 판을 나타내는 배열
   // board[x][y]가 true 인 경우 x, y 위치에 고정된 블록이 존재하는 것을 의미한다
   bool board_[BOARD_WIDTH][BOARD_HEIGHT];
+  // 생성한 테트로미노
+  //Tetromino objectTetromino;
+  // 생성한 테트리미노의 원본
+  //Tetromino originalTetromino;
+  // 다음에 생성할 테트리미노
+  //Tetromino nextTetromino;
+  // 생성한 테트로미노의 x 좌표
+  //int xTetromino;
+  // 생성한 테트로미노의 y 좌표
+  //int yTetromino;
 
 public:
-  // 키를 입력받는다.
-  void handInput();
+  // board_를 초기화한다.
+  void setBoard();
+  
+  // 위쪽 방향키를 누르면 테트로미노를 맨 아래로 이동한다. (상 방향키 입력)
+  void hardDrop();
+
+  // 테트로미노를 일정 간격으로 아래로 이동
+  void softDrop();
+
+  // board를 체크하여 가로 한 줄이 꽉 찼으면 삭제 clearLine에 1을 추가
+  void isClearLine();
+
+  // clearLine이 일정 개수에 도달하면 게임 종료 (gameClear = True)
+  void clearGame();
+
+  // 테트로미노를 시계, 반시계 방향으로 회전한다. (Z, X키 입력)
+  void rotatedTetromino();
+
+  // 테트로미노를 이동한다. (좌, 우, 하 방향키 입력)
+  void moveTetromino();
+
+  // 테트로미노를 생성한다.
+  void createTetromino();
 
   // 게임의 한 프레임을 처리한다.
   void update();
